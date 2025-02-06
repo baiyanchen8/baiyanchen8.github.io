9ee847203986cfbcb3179d2d2d54e40b932dce2d
baiyanchen8 <132753198+baiyanchen8@users.noreply.github.com>
2025-02-06 23:36:33 +0800

    aa

--- start.py
+++ start.py
@@ -112,7 +112,7 @@ def convert_markdown_to_html():
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>{markdown_file.stem}</title>
-            <link rel="stylesheet" href="/style.css">
+            <link rel="stylesheet" href="./style.css">
             <script type="text/javascript" async
                 src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML">
             </script>
